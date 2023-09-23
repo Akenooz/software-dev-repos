1f87980e525b1e15b79355016ea57c70655a275d
Rohit Kumar <rk98991439@gmail.com>
2023-09-23 17:49:06 +0530

    update to main.dart

--- dev/lib/main.dart
+++ dev/lib/main.dart
@@ -1,76 +1,41 @@
 import 'package:flutter/material.dart';
 
+const Color primaryColor = Color.fromARGB(255, 0, 255, 64);
+const TargetPlatform platform = TargetPlatform.android;
+
 void main() {
-  runApp(const MyApp());
+  runApp(DayPlanner());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class DayPlanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        platform: platform,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Day Planer T7'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("📆 Day Planner"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: const [
+              DrawerHeader(
+                child: Center(
+                  child: Text(
+                    "Day Planner 🗓️",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
