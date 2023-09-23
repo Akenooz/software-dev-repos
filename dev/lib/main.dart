530025d779b2822f084a6687b868735566a82024
Rohit Kumar <rk98991439@gmail.com>
2023-09-23 17:58:52 +0530

    working on old code with dark/light theme colored bar

--- dev/lib/main.dart
+++ dev/lib/main.dart
@@ -1,169 +1,122 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'task.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ThemeData lightTheme = ThemeData.light().copyWith(
+    primaryColor: Color.fromARGB(255, 52, 255, 93),
+    platform: TargetPlatform.android,
+    brightness: Brightness.light,
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
+    ),
+    appBarTheme: const AppBarTheme(
+      color: Colors.blue,
+      iconTheme: IconThemeData(color: Colors.white),
+    ),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  );
+
+  final ThemeData darkTheme = ThemeData.dark().copyWith(
+    primaryColor: Color.fromARGB(255, 52, 255, 93),
+    platform: TargetPlatform.android,
+    brightness: Brightness.dark,
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      bodyLarge: TextStyle(fontSize: 18, color: Colors.white),
+    ),
+    appBarTheme: const AppBarTheme(
+      color: Colors.blueGrey,
+      iconTheme: IconThemeData(color: Colors.white),
+    ),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+  );
+
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode =
+      _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const CalendarViewScreen(tasks: []);
-  }
-}
-
-class CalendarViewScreen extends StatefulWidget {
-  final List<Task> tasks;
-
-  const CalendarViewScreen({
-    required this.tasks,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  CalendarViewScreenState createState() => CalendarViewScreenState();
-}
-
-class CalendarViewScreenState extends State<CalendarViewScreen> {
-  final PageController _pageController =
-  PageController(initialPage: DateTime.now().month - 1);
-
-  List<Task> getTasksForDate(DateTime date) {
-    return widget.tasks.where((task) {
-      if (task.deadline != null) {
-        return task.deadline!.year == date.year &&
-            task.deadline!.month == date.month &&
-            task.deadline!.day == date.day;
-      }
-      return false;
-    }).toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calendar View'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    _pageController.previousPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                Text(
-                  DateFormat('MMMM yyyy').format(
-                    DateTime.now().add(
-                      Duration(days: 30 * _pageController.page!.toInt()),
-                    ),
-                  ),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _themeMode,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("📆 Day Planner"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: const [
+              DrawerHeader(
+                child: Center(
+                  child: Text(
+                    "Day Planner 🗓️",
+                    style: TextStyle(fontSize: 32),
                   ),
                 ),
-                IconButton(
-                  onPressed: () {
-                    _pageController.nextPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  icon: const Icon(Icons.arrow_forward),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: 12,
-              itemBuilder: (context, monthIndex) {
-                final currentDate = DateTime.now()
-                    .subtract(Duration(days: DateTime.now().day - 1));
-                final date = currentDate.add(
-                    Duration(days: DateTime.now().day - 1 + 30 * monthIndex));
-
-                final firstDayOfWeek =
-                    DateTime(date.year, date.month, 1).weekday;
-                final lastDayOfMonth =
-                    DateTime(date.year, date.month + 1, 0).day;
-
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7,
-                  ),
-                  itemCount: date.month == DateTime.now().month ? 35 : 42,
-                  itemBuilder: (context, index) {
-                    if (index < firstDayOfWeek - 1 ||
-                        index > lastDayOfMonth + firstDayOfWeek - 2) {
-                      return Container();
-                    }
-
-                    final day = index - firstDayOfWeek + 2;
-
-                    final tasksForDay =
-                    getTasksForDate(DateTime(date.year, date.month, day));
-
-                    return GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                        ),
-                        child: Column(
-                          children: [
-                            if (index == 0 || day == 1)
-                              Text(
-                                DateFormat('MMM').format(date),
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            Text(
-                              '$day',
-                            ),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: tasksForDay.length,
-                                itemBuilder: (context, taskIndex) {
-                                  final task = tasksForDay[taskIndex];
-                                  return ListTile(
-                                    title: Text(task.name),
-                                    subtitle: task.deadline != null
-                                        ? Text(
-                                      DateFormat('hh:mm a')
-                                          .format(task.deadline!),
-                                    )
-                                        : const Text('No deadline'),
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _toggleTheme,
+          child: Icon(_themeMode == ThemeMode.light
+              ? Icons.nightlight_round
+              : Icons.wb_sunny),
+        ),
+        persistentFooterButtons: [
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _themeMode = ThemeMode.light;
+              });
+            },
+            child: Text('Light theme'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _themeMode = ThemeMode.dark;
+              });
+            },
+            child: Text('Dark theme'),
           ),
         ],
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MyApp());
 }
