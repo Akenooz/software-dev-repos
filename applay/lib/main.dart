9bf3a7c90c4ec1ee6d32c25a3ab6ae47d138f3be
Rohit Kumar <rk98991439@gmail.com>
2023-09-25 02:37:43 +0530

    working on task

--- applay/lib/main.dart
+++ applay/lib/main.dart
@@ -1,191 +1,193 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'task.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const CalendarViewScreen(tasks: []);
+    return MaterialApp(
+      home: TaskListPage(),
+    );
   }
 }
 
-class CalendarViewScreen extends StatefulWidget {
-  final List<Task> tasks;
-
-  const CalendarViewScreen({
-    required this.tasks,
-    Key? key,
-  }) : super(key: key);
-
+class TaskListPage extends StatelessWidget {
   @override
-  CalendarViewScreenState createState() => CalendarViewScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task List'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Your Task List Goes Here'),
+            SizedBox(height: 20),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TaskAddingPage()),
+                );
+              },
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class CalendarViewScreenState extends State<CalendarViewScreen> {
-  final PageController _pageController =
-  PageController(initialPage: DateTime.now().month - 1);
-
+class TaskAddingPage extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    _pageController.addListener(_onPageChanged);
+  _TaskAddingPageState createState() => _TaskAddingPageState();
+}
+
+class _TaskAddingPageState extends State<TaskAddingPage> {
+  String title = '';
+  DateTime? selectedDate;
+  TimeOfDay? selectedStartTime;
+  TimeOfDay? selectedEndTime;
+  String location = '';
+  String text = '';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
-  void _onPageChanged() {
-    setState(() {});
+  Future<void> _selectStartTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedStartTime ?? TimeOfDay.now(),
+    );
+    if (picked != null && picked != selectedStartTime) {
+      setState(() {
+        selectedStartTime = picked;
+      });
+    }
   }
 
-  @override
-  void dispose() {
-    _pageController.removeListener(_onPageChanged);
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  String _getFormattedDate() {
-    final currentDate =
-    DateTime.now().subtract(Duration(days: DateTime.now().day - 1));
-    final page = _pageController.page ??
-        0.0; // Use a default value if page is null
-    final date = currentDate.add(Duration(days: (page * 30).toInt()));
-    return DateFormat('MMMM yyyy').format(date);
-  }
-
-  List<Task> getTasksForDate(DateTime date) {
-    return widget.tasks.where((task) {
-      if (task.deadline != null) {
-        return task.deadline!.year == date.year &&
-            task.deadline!.month == date.month &&
-            task.deadline!.day == date.day;
-      }
-      return false;
-    }).toList();
+  Future<void> _selectEndTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedEndTime ?? TimeOfDay.now(),
+    );
+    if (picked != null && picked != selectedEndTime) {
+      setState(() {
+        selectedEndTime = picked;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calendar View'),
+        title: Text('Add Task'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  title = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+            ),
+            Row(
               children: [
-                IconButton(
-                  onPressed: () {
-                    _pageController.previousPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                Text(
-                  _getFormattedDate(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    _pageController.nextPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  icon: const Icon(Icons.arrow_forward),
+                Text('Date: ${selectedDate?.toLocal()}'.split(' ')[0]),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => _selectDate(context),
+                  child: Text('Select Date'),
                 ),
               ],
             ),
-          ),
-          Expanded(
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: 12,
-              itemBuilder: (context, monthIndex) {
-                final currentDate =
-                DateTime.now().subtract(Duration(days: DateTime.now().day - 1));
-                final date = currentDate.add(
-                    Duration(days: DateTime.now().day - 1 + 30 * monthIndex));
-
-                final firstDayOfWeek =
-                    DateTime(date.year, date.month, 1).weekday;
-                final lastDayOfMonth =
-                    DateTime(date.year, date.month + 1, 0).day;
-
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7,
-                  ),
-                  itemCount: date.month == DateTime.now().month ? 35 : 42,
-                  itemBuilder: (context, index) {
-                    if (index < firstDayOfWeek - 1 ||
-                        index > lastDayOfMonth + firstDayOfWeek - 2) {
-                      return Container();
-                    }
-
-                    final day = index - firstDayOfWeek + 2;
-
-                    final tasksForDay =
-                    getTasksForDate(DateTime(date.year, date.month, day));
-
-                    return GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                        ),
-                        child: Column(
-                          children: [
-                            if (index == 0 || day == 1)
-                              Text(
-                                DateFormat('MMM').format(date),
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            Text(
-                              '$day',
-                            ),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: tasksForDay.length,
-                                itemBuilder: (context, taskIndex) {
-                                  final task = tasksForDay[taskIndex];
-                                  return ListTile(
-                                    title: Text(task.name),
-                                    subtitle: task.deadline != null
-                                        ? Text(
-                                      DateFormat('hh:mm a')
-                                          .format(task.deadline!),
-                                    )
-                                        : const Text('No deadline'),
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
+            Row(
+              children: [
+                Text('Start Time: ${selectedStartTime?.format(context) ?? ''}'),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => _selectStartTime(context),
+                  child: Text('Select'),
+                ),
+              ],
             ),
-          ),
-        ],
+            Row(
+              children: [
+                Text('End Time: ${selectedEndTime?.format(context) ?? ''}'),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => _selectEndTime(context),
+                  child: Text('Select'),
+                ),
+              ],
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  location = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Location',
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  text = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Text',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Add the task to your task list or database
+                // You can use the title, selectedDate, selectedStartTime, selectedEndTime, location, and text variables
+                // to access the entered values and save them.
+                // For example, you can call a function like addTask(title, selectedDate, selectedStartTime, selectedEndTime, location, text)
+              },
+              child: Text('Add Task'),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+void addTask(
+    String title,
+    DateTime? selectedDate,
+    TimeOfDay? selectedStartTime,
+    TimeOfDay? selectedEndTime,
+    String location,
+    String text,
+    ) {
+  // Implement your task addition logic here
 }
