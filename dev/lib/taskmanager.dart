fc020caacdc53a467053a146ee5cba025b3da01f
DivleenSR <145339236+DivleenSR@users.noreply.github.com>
2023-09-27 00:39:58 +0530

    Add files via upload

---
+++ dev/lib/taskmanager.dart
@@ -0,0 +1,24 @@
+class Task {
+  final String name;
+  final DateTime? deadline; // Nullable DateTime
+
+  Task({
+    required this.name,
+    this.deadline,
+  });
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      name: json['name'] ?? '',
+      deadline:
+      json['deadline'] != null ? DateTime.parse(json['deadline']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+    'name': name,
+    'deadline': deadline?.toIso8601String(),
+    };
+  }
+}
