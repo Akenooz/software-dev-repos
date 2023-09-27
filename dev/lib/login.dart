b1fb8ce19ee93352f887bf76abe6fe0a13fcd614
Rohit Kumar <rk98991439@gmail.com>
2023-09-27 07:15:02 +0530

    login file improved but backend not done yet

--- dev/lib/login.dart
+++ dev/lib/login.dart
@@ -40,10 +40,30 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  Widget buildTextField(String labelText, bool obscureText) {
+    return PhysicalModel(
+      color: Colors.white,
+      elevation: 4,
+      shadowColor: Colors.grey.withOpacity(0.5),
+      borderRadius: BorderRadius.circular(8),
+      child: TextFormField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.symmetric(
+            vertical: 15,
+            horizontal: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFFF3F6FB), // Background color
       appBar: AppBar(
         title: Text('Login Page'),
       ),
@@ -55,16 +75,17 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 50),
 
               // Day Planner icon
-              const Icon(
+              Icon(
                 Icons.calendar_today, // Replace with your desired icon
                 size: 100,
+                color: Color(0xFF007BFF), // Icon color
               ),
 
               const SizedBox(height: 50),
 
               // Welcome text
               Text(
-                'Welcome back you\'ve been missed!',
+                'Welcome back, you\'ve been missed!',
                 style: TextStyle(
                   color: Colors.grey[700],
                   fontSize: 16,
@@ -76,22 +97,11 @@ class _LoginPageState extends State<LoginPage> {
               // Email and Password Fields
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
+                child: buildTextField('Email', false),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
+                child: buildTextField('Password', true),
               ),
 
               const SizedBox(height: 10),
@@ -113,29 +123,76 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 25),
 
               // Login Button
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => MyApp()),
-                  );
-                },
-                child: Text('Login'),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF2E78C7), Color(0xFF00BFFF)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent, // Button color
+                    elevation: 0, // Remove button shadow
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
               ),
 
               SizedBox(height: 20),
 
               // Google Sign-In Button
-              ElevatedButton.icon(
-                onPressed: _handleSignIn,
-                icon: Image.asset(
-                  'lib/images/google.png', // Make sure this path is correct
-                  width: 24,
-                  height: 24,
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.grey],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
                 ),
-                label: Text(''),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // Background color
-                  onPrimary: Colors.black, // Text color
+                child: ElevatedButton.icon(
+                  onPressed: _handleSignIn,
+                  icon: Image.asset(
+                    'lib/images/google.png', // Make sure this path is correct
+                    width: 24,
+                    height: 24,
+                  ),
+                  label: Text(
+                    'Sign In with Google',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent, // Button color
+                    elevation: 0, // Remove button shadow
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
                 ),
               ),
             ],
